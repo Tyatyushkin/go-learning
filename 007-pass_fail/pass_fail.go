0c369675e2a1c88b646f53b020036f5c883c247f
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2022-08-14 16:15:04 +0300

    add task pass_fail without err

---
+++ 007-pass_fail/pass_fail.go
@@ -0,0 +1,15 @@
+// pass_fail сообщает, сдал ли пользователь экзамен.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Print("Enter a grade: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	fmt.Println(input)
+}
