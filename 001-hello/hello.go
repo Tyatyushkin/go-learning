5731de7abc54814c9b4edbebb77bb40403f7fb4a
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2022-08-15 22:45:07 +0300

    modify hello_test.go

--- 001-hello/hello.go
+++ 001-hello/hello.go
@@ -2,10 +2,13 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello, Go!"
+func Hello(name string) string {
+	if name == "" {
+		name = "GO"
+	}
+	return "Hello, " + name
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("GO"))
 }
