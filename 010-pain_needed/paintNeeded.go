ea05787158fec51dc19cc2166c77ccdafdc24ade
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2022-08-23 14:59:16 +0300

    modift paintNeeded

--- 010-pain_needed/paintNeeded.go
+++ 010-pain_needed/paintNeeded.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
-func paintNeeded(width float64, height float64) {
+func paintNeeded(width float64, height float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("a width of %0.2f is invalid", width)
+	}
+	if height < 0 {
+		return 0, fmt.Errorf("a width of %0.2f is invalid", height)
+	}
 	area := width * height
-	fmt.Printf("%.2f litres need\n", area/10.0)
+	return area / 10.0, nil
 }
 
 func main() {
-	paintNeeded(4.2, 3.0)
-	paintNeeded(5.2, 3.5)
-	paintNeeded(5.0, 3.3)
+	amount, err := paintNeeded(4.2, -3.0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%0.2f litres needed\n", amount)
 }
