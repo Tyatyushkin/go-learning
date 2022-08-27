e4ffdd4a915a804e5f8a0e153b0e8d3c67a108ef
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2022-08-27 14:01:10 +0300

    add subscriber task for struct with external module

--- 016-subscriber/subscriber.go
+++ 016-subscriber/subscriber.go
@@ -7,7 +7,23 @@ import (
 )
 
 func main() {
-	var s magazine.Subscriber
-	s.Rate = 4.99
-	fmt.Println(s.Rate)
+	subscriber := magazine.Subscriber{Name: "Aman Singh"}
+	subscriber.Street = "123 Oak St"
+	subscriber.City = "Omaha"
+	subscriber.State = "NE"
+	subscriber.PostalCode = "68111"
+	fmt.Println("Street: ", subscriber.Street)
+	fmt.Println("City: ", subscriber.City)
+	fmt.Println("State: ", subscriber.State)
+	fmt.Println("Postal  Code: ", subscriber.PostalCode)
+
+	employee := magazine.Employee{Name: "Joy Carr"}
+	employee.Street = "456 Elm St"
+	employee.City = "Portland"
+	employee.State = "OR"
+	employee.PostalCode = "97222"
+	fmt.Println("Street: ", employee.Street)
+	fmt.Println("City: ", employee.City)
+	fmt.Println("State: ", employee.State)
+	fmt.Println("Postal  Code: ", employee.PostalCode)
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.19
 
 require github.com/headfirstgo/keyboard v0.0.0-20170926053303-9930bcf72703
 
-require github.com/tyatyushkin/magazine v0.0.0-20220827083239-908cb02b6f86
+require github.com/tyatyushkin/magazine v0.0.0-20220827084829-4aa19f5849e8
